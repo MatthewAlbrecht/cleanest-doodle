4ee5516d5f6ffb6816440564b8a773b469d0a088
Matthew Albrecht <matthewwilliamalbrecht@gmail.com>
2022-01-14 23:58:24 -0500

    update styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,8 +5,10 @@ import {
   Form,
   json,
   redirect,
+  useActionData,
   useLoaderData,
   useSubmit,
+  useTransition,
 } from 'remix';
 import { db } from '~/utils/db.server';
 
@@ -27,7 +29,7 @@ export const loader = async () => {
   let count = await db.doodle.count();
   let leaderboardData = await db.doodle.findMany({
     orderBy: [{ votesFor: { _count: 'desc' } }],
-    take: 15,
+    take: 100,
     select: {
       tokenId: true,
       imageUrl: true,
@@ -109,6 +111,9 @@ export const action: ActionFunction = async ({ request }) => {
 export default function IndexRoute() {
   const submit = useSubmit();
   const data = useLoaderData<LoaderData>();
+  const action = useActionData<ActionData>();
+  const transition = useTransition();
+  console.log(transition.submission);
 
   async function handleChange(e: React.FormEvent<HTMLFormElement>) {
     submit(e.currentTarget, { replace: true });
@@ -128,7 +133,7 @@ export default function IndexRoute() {
         </nav>
       </header>
       <main className="mt-12 container mx-auto">
-        <h2 className="text-5xl text-white text-center">
+        <h2 className="text-3xl md:text-5xl text-white text-center">
           Which dood is{' '}
           <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-doodles-green via-doodles-orange to-doodles-pink">
             cleaner?
@@ -136,10 +141,10 @@ export default function IndexRoute() {
         </h2>
         <Form method="post" onChange={handleChange}>
           {data?.dood1 && data.dood2 && (
-            <div className="m-16">
+            <div className="m-6 my-12 md:m-16">
               <input type="hidden" name="dood-1-id" value={data.dood1.id} />
               <input type="hidden" name="dood-2-id" value={data.dood2.id} />
-              <div className="grid gap-20 grid-cols-2">
+              <div className="grid gap-8 md:gap-20 grid-cols-2">
                 <DoodleRadio dood={data.dood1} />
                 <DoodleRadio dood={data.dood2} />
               </div>
@@ -147,10 +152,10 @@ export default function IndexRoute() {
           )}
         </Form>
         <div className="mt-20">
-          <h2 className="text-5xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-doodles-green via-doodles-orange to-doodles-pink">
+          <h2 className="text-3xl md:text-5xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-doodles-green via-doodles-orange to-doodles-pink">
             Leaderboards
           </h2>
-          <ul className="mt-8 mx-auto max-w-md divide-y-2 divide-white/25">
+          <ul className="px-4 sm:px-0 mt-8 mx-auto max-w-md divide-y-2 divide-white/25">
             {data.leaderboard.map((dood, index) => (
               <li
                 key={dood.tokenId}
@@ -192,7 +197,7 @@ function DoodleRadio({
           name="doodle-radio"
           value={dood.id}
         />
-        <div className="bg-white hover:bg-white/95 p-4 rounded-lg peer-checked:p-2 peer-checked:border-8 peer-checked:border-doodles-green">
+        <div className="bg-white md:hover:bg-white/95 p-2 sm:p-4 rounded-lg md:peer-checked:p-2 md:peer-checked:border-8 md:peer-checked:border-doodles-green">
           <img className="w-auto rounded-lg" src={`${dood.imageUrl}=s512`} />
           <h3 className="py-4 -mb-4 text-right leading-none text-xl font-bold text-indigo-900">
             #{dood.tokenId}
